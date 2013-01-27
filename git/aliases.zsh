8c8cd6b64f9e5835725110a0604b6f39c4d66dd7
Jon Crooke <joncrooke@gmail.com>
2013-01-27 14:07:42 +0100

    My changes
    My info
    
    Opendiff
    
    Hub config
    
    Hub user
    
    Alias to hub
    
    use git://
    
    Xclean
    
    Git ignore
    
    xclean
    
    gprune

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,7 +3,7 @@ g () {
   then
     git status -sb
   else
-    git $*
+    hub $*
   fi
 }
 compdef g=git
@@ -13,3 +13,6 @@ compdef _git gl=git-log
 
 alias gs='git status'
 compdef _git gs=git-status
+
+alias xclean='rm\ -rf\ ~/Library/Developer/Xcode/DerivedData/'
+alias gprune='git branch --merged | grep -v \* | xargs git branch -D'
