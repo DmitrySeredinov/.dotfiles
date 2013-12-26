c6fd6389b455537c4fcd650def29045dd3e69551
Jonathan Crooke <joncrooke@gmail.com>
2013-12-26 12:02:11 +0000

    Script

---
+++ dropbox/dropbox.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+HOME=~/DropBox\ Other/Stylemarks /Applications/Dropbox.app/Contents/MacOS/Dropbox &> /dev/null &
+HOME=~/DropBox\ Other/Memrise /Applications/Dropbox.app/Contents/MacOS/Dropbox &> /dev/null &
