7b6db0989967b34093dd4852475168f656ec7de3
Jonathan Crooke <joncrooke@gmail.com>
2014-01-07 12:10:35 +0100

    Tidy aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -13,6 +13,3 @@ compdef _git gl=git-log
 
 alias gs='git status'
 compdef _git gs=git-status
-
-alias xclean='rm\ -rf\ ~/Library/Developer/Xcode/DerivedData/'
-alias gprune='git branch --merged | grep -v \* | xargs git branch -D'
--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+command -v xcodebuild >/dev/null 2>&1 || { exit 0; }
+
 alias ios="open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app"
+alias xclean='rm\ -rf\ ~/Library/Developer/Xcode/DerivedData/'
