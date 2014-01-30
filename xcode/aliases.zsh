ec5a37994da7e96d42d79bf92090136f78c78424
Jonathan Crooke <joncrooke@gmail.com>
2014-01-30 11:14:18 +0100

    rmpods

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -4,3 +4,4 @@ command -v xcodebuild >/dev/null 2>&1 || { exit 0; }
 
 alias ios="open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app"
 alias xclean='rm\ -rf\ ~/Library/Developer/Xcode/DerivedData/'
+alias rmpods='rm -rf Pods && rm Podfile.lock'
