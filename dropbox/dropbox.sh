914ccd69b13a462412f0b4989c5849cb9159124c
Jonathan Crooke <joncrooke@gmail.com>
2014-04-12 12:44:21 +0200

    Remove dropbox

--- dropbox/dropbox.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-HOME=~/DropBox\ Other/Stylemarks /Applications/Dropbox.app/Contents/MacOS/Dropbox &
-HOME=~/DropBox\ Other/Memrise /Applications/Dropbox.app/Contents/MacOS/Dropbox &
-
