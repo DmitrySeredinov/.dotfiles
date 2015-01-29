4bb38141563c519f4c665eb08691a545038fa657
Jonathan Crooke <joncrooke@gmail.com>
2015-01-29 15:17:19 +0100

    Separate work aliases

---
+++ zsh/work.zsh
@@ -0,0 +1,3 @@
+# Project related
+alias e='eval D=~/dev/EyeEm/EyeEm && if [[ `pwd` != "$D" ]]; then pushd "$D"; fi'
+alias ec='eval D=~/dev/EyeEm/EyeEm/eyeem-ios-submodules/EyeEmCamera && if [[ `pwd` != "$D" ]]; then pushd "$D"; fi'
