cf5478e33b2ba2b3aefa846e672b62d276327806
Jonathan Crooke <joncrooke@gmail.com>
2015-01-29 17:00:09 +0100

    Library

--- zsh/work.zsh
+++ zsh/work.zsh
@@ -1,3 +1,4 @@
 # Project related
 alias e='eval D=~/dev/EyeEm/EyeEm && if [[ `pwd` != "$D" ]]; then pushd "$D"; fi'
 alias ec='eval D=~/dev/EyeEm/EyeEm/eyeem-ios-submodules/EyeEmCamera && if [[ `pwd` != "$D" ]]; then pushd "$D"; fi'
+alias el='eval D=~/dev/EyeEm/EyeEm/eyeem-ios-submodules/EyeEmLibrary && if [[ `pwd` != "$D" ]]; then pushd "$D"; fi'
